a8475930e67ae9509e0a5a70c8bdf85858047bd9
Bill <wherrera10@users.noreply.github.com>
2018-01-25 22:17:22 -1000

    update for module changes

--- test/test1.jl
+++ test/test1.jl
@@ -1,13 +1,11 @@
-using OpenBCI_WiFI.jl
+using OpenBCI_WiFi
 
-# set these for your systems
-
-boardIP = "192.168.1.25"
+boardIP = "192.168.1.23"
 myIP = "192.168.1.1"
 idfile = "patientdata.json"
 
-
-makeganglionbdfplus("testfile.bdf", boardIP, myIP, idfile=idfile)
+OpenBCI_WiFi.makeganglionbdfplus("testfile.bdf", boardIP, myIP, 30, idfile=idfile)
 
 
 true
+
