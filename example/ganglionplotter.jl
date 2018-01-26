a22db2a020e8818bccc884c32be198dd4b764cbc
Bill <wherrera10@users.noreply.github.com>
2018-01-25 22:10:45 -1000

    update for changes in module

--- example/ganglionplotter.jl
+++ example/ganglionplotter.jl
@@ -14,7 +14,7 @@ using PyPlot
 # ENV["MPLBACKEND"]="qt4agg" # set backend for PyPlot as needed
 
 
-boardIP = "192.168.1.25"
+boardIP = "192.168.1.23"
 myIP = "192.168.1.1"
 idfile = "../test/patientdata.json"
 
@@ -22,27 +22,32 @@ const PLOTINTERVAL = 10
 
 
 function plottwobipolars(bdfh, pcount, maxpackets)
-    if pcount % PLOTINTERVAL == 0
-        startrec = pcount - PLOTINTERVAL + 1
-        cdata = multichanneltimesegment(edfh, [1,2,3,4], startrec, pcount, physical)
+    if pcount >= PLOTINTERVAL && pcount % PLOTINTERVAL == 0
+        startrec = pcount - PLOTINTERVAL + 1.0
+        cdata = multichanneltimesegment(bdfh, [1,2,3,4], startrec, pcount + 0.0, true)
         Fp1T3 = cdata[2] .- cdata[1]
         Fp2T4 = cdata[4] .- cdata[3]
-        Fp1T3 = EDFPlus.lowpassfilter(Fp1T3, 250, cutoff=0.5)
-        Fp2T4 = EDFPlus.lowpassfilter(Fp2T4, 250, cutoff=0.5)
+        Fp1T3 = EDFPlus.lowpassfilter(Fp1T3, 250)
+        Fp2T4 = EDFPlus.lowpassfilter(Fp2T4, 250)
+        Fp1T3 = EDFPlus.highpassfilter(Fp1T3, 250)
+        Fp2T4 = EDFPlus.highpassfilter(Fp2T4, 250)
         ydata = [Fp1T3, Fp2T4]
         timepoints = linspace(0.0, PLOTINTERVAL, length(Fp1T3))
         
+        # detach plotting, return quickly now so as to avoid dropped packets
+        @async(begin
         plt = Plots.plot(timepoints, ydata, layout=(2,1),
                    xticks=collect(timepoints[1]:1:timepoints[end]), 
                    yticks=false, legend=false, title="Interval from $startrec to $pcount")
         plt[1][:xaxis][:showaxis] = false
         Plots.plot!(yaxis=true, tight_layout=true, ylabel = "Fp1-T3", subplot=1)
         Plots.plot!(yaxis=true, tight_layout=true, ylabel = "Fp2-T4", subplot=2)
-        PyPlot.display(plt)   
+        PyPlot.display(plt)
+        end)
     end
 end
 
 
-OpenBCI_WiFi.makeganglionbdfplus("examplefile.bdf", boardIP, myIP, idfile=idfile, 
-                     packetinspector=plottwobipolars, maxpackets=120)
+OpenBCI_WiFi.makeganglionbdfplus("examplefile.bdf", boardIP, myIP, 120,
+                             idfile=idfile, inspector=plottwobipolars)
 
